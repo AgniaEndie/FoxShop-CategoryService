f81127c7be5a32812cf87730082a4ec2dfd6103f
agniaendie <endienasg3@gmail.com>
2024-07-21 03:15:44 +0500

    Test RedisConfig for new Architecture of Redis Cluster

--- src/main/kotlin/ru/agniaendie/categoryservice/config/RedisConfig.kt
+++ src/main/kotlin/ru/agniaendie/categoryservice/config/RedisConfig.kt
@@ -1,27 +1,46 @@
 package ru.agniaendie.categoryservice.config
 
+import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+import org.springframework.cache.annotation.CachingConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisSentinelConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
 
 @Configuration
-class RedisConfig(@Value("\${cache.redis.host}") val redisHost: String,@Value("\${cache.redis.port}") val redisPort: Int) {
+@RequiredArgsConstructor
+class RedisConfig(
+    @Value("\${spring.cache.redis.time-to-live}") var redisTimeToLive: Long,
+    @Value("\${spring.data.redis.timeout}") var redisCommandTimeout: Duration,
+    val redisProperties: RedisProperties
+) : CachingConfigurer {
+
+
     @Bean
-    fun jedisConnectionFactory(): JedisConnectionFactory {
-        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
-        redisStandaloneConfiguration.hostName = redisHost
-        redisStandaloneConfiguration.port = redisPort
-        return JedisConnectionFactory(redisStandaloneConfiguration)
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        val redisSentinelConfiguration = RedisSentinelConfiguration()
+        redisSentinelConfiguration.master(redisProperties.sentinel.master)
+        redisProperties.sentinel.nodes.forEach { s -> redisSentinelConfiguration.sentinel(s, redisProperties.port) }
+        redisSentinelConfiguration.password = RedisPassword.of(redisProperties.password)
+
+//        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+//        redisStandaloneConfiguration.hostName = redisHost
+//        redisStandaloneConfiguration.port = redisPort
+
+
+        val factory = LettuceConnectionFactory(redisSentinelConfiguration)
+        return factory
     }
 
     @Bean
     fun redisTemplate(): RedisTemplate<String, Any> {
         val redisTemplate = RedisTemplate<String, Any>()
-        redisTemplate.connectionFactory = jedisConnectionFactory()
+        redisTemplate.connectionFactory = redisConnectionFactory()
         return redisTemplate
     }
 }
