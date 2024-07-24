11f9384d7a1abc66ee0cfadd68249d81b064f181
agniaendie <endienasg3@gmail.com>
2024-07-24 20:19:04 +0500

    Add Index to Database

--- src/main/resources/db.migration/V01__init.sql
+++ src/main/resources/db.migration/V01__init.sql
@@ -2,4 +2,5 @@ create table if not exists category (
     uuid varchar(256) primary key,
     title varchar(256),
     hide boolean
-)
+);
+create index if not exists category_name_index on category(title)
